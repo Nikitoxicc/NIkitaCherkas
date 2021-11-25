074d663a78134c52e3efd4841c551b897b60ff8a
Nikitoxicc <91672927+Nikitoxicc@users.noreply.github.com>
2021-11-25 19:55:35 +0000

    Правки

--- Task 3-1.cpp
+++ Task 3-1.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 /**
 * \brief Математическая функция, рассчитывающая значение функции y
-* \param const double x - константа, имеющая тип данных с плавающей точкой двойной точности
-* \param const double q - константа, заданная по условию
+* \param x - константа, имеющая тип данных с плавающей точкой двойной точности
+* \param q - константа, заданная по условию
 * \return Возвращает значение функции y
 **/
 
-double gety(const double x, const double q);
+double getY(const double x, const double q);
 
 /**
 * \brief Точка входа в программу.
@@ -19,15 +19,14 @@ double gety(const double x, const double q);
 int main()
 {
     const double x = 0;
-    const double q = 1/3;
-    const auto y = gety(x,q);
-}
-
-double gety(const double x, const double q)
-{
+    const double q = 1.0 / 3;
     for (double x = 0; x <= 0.8; x = x + 0.01)
     {
-        cout << "y = " << tan(x)- q*pow(tan(x),3) + 0.2 * pow(tan(x),5) - q << endl;
+        cout << "y = " << getY(x,q) << endl;
     }
-    return 1;
+}
+
+double getY(const double x, const double q)
+{
+    return tan(x) - q * pow(tan(x), 3 ) + 0.2 * pow(tan(x), 5 ) - q;
 }

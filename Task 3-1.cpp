50ba25e19e90d41cd015be6267885f28510aa246
Nikitoxicc <91672927+Nikitoxicc@users.noreply.github.com>
2021-12-09 22:30:16 +0300

    Update Task 3-1.cpp

--- Task 3-1.cpp
+++ Task 3-1.cpp
@@ -1,32 +1,45 @@
 #include <cmath>
 #include <iostream>
-
 using namespace std;
 
 /**
-* \brief Математическая функция, рассчитывающая значение функции y
-* \param x - константа, имеющая тип данных с плавающей точкой двойной точности
-* \param q - константа, заданная по условию
-* \return Возвращает значение функции y
+ * \brief Проверка косинуса на равенство 0.
+ *\param x - константа, имеющая тип данных с плавающей точкой двойной точности.
+ * return 0 - в случае неравенства 0.
 **/
-
-double getY(const double x, const double q);
+double isCalculated(const double x);
 
 /**
+* \brief Математическая функция, рассчитывающая значение функции y.
+* \param x - константа, имеющая тип данных с плавающей точкой двойной точности.
+* \return Возвращает значение функции y.
+**/
+double getY(const double x);
+/**
 * \brief Точка входа в программу.
+* \param step - шаг.
+* \param border - Максимальное значение x.
+* \param value1 - Начальное значение x.
 * \return Возвращает 0 в случае успешного выполнения.
 */
 int main()
 {
     const double x = 0;
-    const double q = 1.0 / 3;
-    for (double x = 0; x <= 0.8; x = x + 0.01)
+    const double step = 0.05;
+    const double border = 0.8;
+    const int value1 = 0;
+    for (double x = value1; x <= border; x = x + step)
     {
-        cout << "y = " << getY(x,q) << endl;
+        cout << "y = " << getY(x) << endl;
     }
 }
 
-double getY(const double x, const double q)
-{
-    return tan(x) - q * pow(tan(x), 3 ) + 0.2 * pow(tan(x), 5 ) - q;
+double isCalculated(const double x){
+    const double epsilon = 0.001;
+    if ((cos(x) - 0) <= epsilon)
+      return 0;
 }
+
+double getY(const double x) {
+    if (isCalculated(x) == 0)
+     return tan(x) - 1.0 / 3.0 * pow(tan(x), 3 ) + 0.2 * pow(tan(x), 5 ) - 1.0 / 3.0;}

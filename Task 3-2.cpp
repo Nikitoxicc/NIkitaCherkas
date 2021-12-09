4b14926978dc8974d3d6bc6cb6732f9a835fd8e5
Nikitoxicc <91672927+Nikitoxicc@users.noreply.github.com>
2021-12-09 23:01:36 +0300

    Update Task 3-2.cpp

--- Task 3-2.cpp
+++ Task 3-2.cpp
@@ -3,18 +3,11 @@
 using namespace std;
 
 /**
- * \breif функция подсчёта факториала.
- * param k - переменная для вычисления элемента ряда.
- * \return значение факториала.
-*/
-double Faktorial(const double k);
-
-/**
  * \brief функция определения текущего элемента ряда.
  * param k - переменная для вычисления элемента ряда.
  * \return значение текущего элемент ряда.
 */
-double getCurrent(const double k);
+double getCurrent(const int k);
 
 /**
  * \brief функция вычисления суммы первых n элементов ряда.
@@ -22,7 +15,7 @@ double getCurrent(const double k);
  * param k - переменная для вычисления элемента ряда.
  * \return сумма первых n элементов ряда.
  */
-double getSumm1(int n, double k);
+double getSumm1(int n, int k);
 
 /**
  * \brief вычисление суммы членов ряда не меньших числа e.
@@ -30,7 +23,7 @@ double getSumm1(int n, double k);
  * param k - переменная для вычисление элемента ряда.
  * \return сумма членов ряда не меньших числа e.
 */
-double getSumm2(double e, double k);
+double getSumm2(double e, int k);
 
 /**
  * \brief точка входа в программу.
@@ -38,45 +31,38 @@ double getSumm2(double e, double k);
 */
 int main()
 {
-int n;
-double e = 0.0;
-double k = 0.0;
-cout<<"Введите количество членов ряда"<< endl;
-cin >> n;
-cout<< "Введите e ";
-cin>>e;
-cout<< getSumm1(n, k)<< endl;
-cout<< getSumm2(e, k);
-return 0;
+    int n;
+    double e = 0.0;
+    int k = 0.0;
+    cout<<"Введите количество членов ряда"<< endl;
+    cin >> n;
+    cout<< "Введите e ";
+    cin>>e;
+    cout<< getSumm1(n, k)<< endl;
+    cout<< getSumm2(e, k);
+    return 0;
 }
 
-double Faktorial(const double k){
-if (k == 0)
-return 1;
-else
-return k * Faktorial(k-1);
+double getCurrent(const int k){
+    return 1.0 / (2.0 * k + 1);
 }
 
-double getCurrent(const double k){
-return 1.0 / Faktorial(2.0 * k);
-}
-
-double getSumm1(int n,double k){
-double sum;
-for (int i = 1; i <=  n; i++){
-sum = sum + getCurrent(k);
-k=k+1;
-}
+double getSumm1(int n,int k){
+    double sum;
+    for (int i = 1; i <=  n; i++){
+        sum = sum + getCurrent(k);
+        k=k+1;
+    }
 return sum;
 }
 
-double getSumm2(double e, double k){
-double cur, sum;
-cur = getCurrent(k);
-while (cur > e){
-sum = sum + cur;
-k++;
-cur = getCurrent(k);
-}
-return sum;
+double getSumm2(double e, int k){
+    double cur, sum;
+    cur = getCurrent(k);
+    while (cur > e){
+        sum = sum + cur;
+        k++;
+        cur = getCurrent(k);
+    }
+    return sum;
 }

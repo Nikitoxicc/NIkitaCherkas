dac5c27c8052781086ef668c70449cee0c3db0ea
Nikitoxicc <91672927+Nikitoxicc@users.noreply.github.com>
2021-12-25 11:22:53 +0000

    Task 4-2

---
+++ Task 4-2.cpp
@@ -0,0 +1,246 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+
+using namespace std;
+
+/**
+ * \brief Проверка ввода размера массива.
+ * \return Размер массива.
+ */
+size_t GetSize();
+
+/**
+ * \brief Заполнение массива случайными числами.
+ * \param size размер массива.
+ * \param minValue минимальное значение элементов массива.
+ * \param maxValue максимальное значение элементов массива.
+ * \return заполненный массив.
+ */
+int* FillRandomArray(size_t size, int minValue, int maxValue);
+
+/**
+ * \brief Вывод массива на консоль.
+ * \param myArray массив.
+ * \param size размер массива.
+ */
+void ArrayPrint(const int* myArray, size_t size);
+
+/**
+ * \brief 
+ * \param 
+ * \param .
+ * \return .
+ */
+void IsOddAndMulpiples3(int* &myArray, size_t &size);
+
+/**
+ * \brief 
+ * \param 
+ * \param .
+ * \return .
+ */
+void ArrayChange(int* &myArray, size_t &size);
+
+/**
+ * \brief Замена минимального элемента массива на средний.
+ * \param myArray массив.
+ * \param size размер массива.
+ * \param maxValue максимальное значение, которое может принимать элемент массива.
+ */
+void MinToLastChange(int* myArray, const size_t size, const int maxValue);
+
+/**
+ * \brief Метод, возвращающий заполненный пользователем массив.
+ * \param size размер массива.
+ * \param minValue минимальное значение элементов массива.
+ * \param maxValue максимальное значение элементов массива.
+ * \return заполненный массив.
+ */
+int* FillUserArray(size_t size);
+
+/**
+ * \brief Варианы ввода массива.
+ */
+enum class ArrayInputWay
+{
+    random,
+    keyboard
+};
+
+/**
+ * \brief Точка входа в программу.
+ * \return 0, в случае успеха.
+ */
+int main()
+{
+    size_t size = GetSize();
+
+    if (size == 0)
+        return 1;
+        
+    cout << "Как вы хотите заполнить массив?\n";
+    cout << static_cast<int>(ArrayInputWay::random) << " - random,\n";
+    cout << static_cast<int>(ArrayInputWay::keyboard) << " - keyboard.\n";
+    cout << "Ваш выбор: ";
+    int choice;
+    cin >> choice;
+        
+    const auto chosen = static_cast<ArrayInputWay>(choice);
+    int* myArray = nullptr;
+    
+    auto minValue = 0;
+    auto maxValue = 0;
+    cout << "Введите диапазон чисел массива (сначала минимум, потом максимум) " << endl;
+    cin >> minValue >> maxValue;
+    if (maxValue <= minValue)
+    {
+        cout << "Введен неправильный диапазон!" << endl;
+    }
+    
+    switch (chosen)
+    {
+    case ArrayInputWay::random:
+    {
+        myArray = FillRandomArray(size, minValue, maxValue);
+        break;
+    }
+    case ArrayInputWay::keyboard:
+    {
+        myArray = FillUserArray(size);
+        break;
+    }
+    }
+    
+    cout << "Массив с заменённым минимальным элементом: "<<endl;
+        
+    MinToLastChange(myArray, size, maxValue);
+    ArrayPrint(myArray, size);
+    
+    cout<< "Массив с удалёнными нечетными элементами, кратными 3м"<<endl;
+    IsOddAndMulpiples3(myArray,size);
+    ArrayPrint(myArray,size);
+    
+    cout<< "Заменённый массив по формулам A[i] = C[i] + i и A[i] = C[i] - i" <<endl;
+    ArrayChange(myArray,size);
+    ArrayPrint(myArray, size);
+    if (myArray != nullptr) { 
+            
+        delete[] myArray;
+        myArray = nullptr;
+        
+    }
+    return 0;
+    
+}
+
+size_t GetSize(){
+    int size = 0;
+    cout << "Введите размер массива"<< endl;
+    cin >> size;
+    if (size <= 0)
+    {
+        cout<< "Введён неверный размер";
+        return 0;
+    }
+    else 
+     return size;
+};
+
+void MinToLastChange(int* myArray, const size_t size, const int maxValue )
+{
+    int temprary = 0;
+    if (myArray == nullptr)
+        cout << "Массив пуст";
+
+    auto minArrayValue = maxValue;
+    auto minElementIndex = 0;
+
+    for (size_t index = 0; index < size; index++) {
+        if (abs(myArray[index]) < minArrayValue) {
+            minArrayValue = abs(myArray[index]);
+            minElementIndex = index;
+        }
+    }
+    
+    temprary = myArray[minElementIndex];
+    myArray[minElementIndex] = myArray[size - 1];
+    myArray[size - 1] = temprary;
+}
+
+void ArrayChange(int* &myArray, size_t &size){
+    
+    int* newArray = new int[size];
+    for (size_t index = 0, newindex = 0; index < size; index++) {
+        if (myArray[index] % 2 == 1){
+            newArray[newindex] = myArray[index] - index;
+        }
+        else {
+            newArray[newindex] = myArray[index] + index;
+        }
+        newindex++;
+    }
+    delete []myArray;
+    myArray = newArray;
+}
+
+void IsOddAndMulpiples3(int* &myArray, size_t &size){
+    
+    size_t count = 0;
+    for (size_t index = 0; index < size; index++) {
+        if (myArray[index] % 2 == 1 && myArray[index] % 3 == 0){
+            count++;
+        }
+    }
+    
+    int* newArray = new int[size - count];
+    for (size_t index = 0, newindex = 0; index < size; index++) {
+        if (!(myArray[index] % 2 == 1 && myArray[index] % 3 == 0)){
+            newArray[newindex] = myArray[index];
+            newindex++;
+        }
+    }
+    delete []myArray;
+    myArray = newArray;
+    size = size - count;
+}
+
+void ArrayPrint(const int* myArray, const size_t size)
+{
+    if (myArray == nullptr)
+    {
+        cout << "Массива не существует";
+    }
+    else {
+        cout << "Массив:\n";
+        for (size_t index = 0; index < size; index++) {
+            cout << myArray[index] << " ";
+        }
+        cout << "\n";
+
+    }
+}
+
+int* FillRandomArray(const size_t size, const int minValue, const int maxValue)
+{
+    srand(time(NULL));
+    const auto area = abs(minValue) + abs(maxValue) + 1;
+    auto* array = new int[size];
+    for (size_t index = 0; index < size; index++) {
+        array[index] = rand() % area + minValue;
+        cout << array[index] << endl;
+    }
+
+    return array;
+}
+
+int* FillUserArray(const size_t size)
+{
+    auto* array = new int[size];
+    cout << "Введите элементы массива" << "\n";
+    for (size_t index = 0; index < size; index++)
+    {
+        cin >> array[index];
+    }
+    return array;
+}
